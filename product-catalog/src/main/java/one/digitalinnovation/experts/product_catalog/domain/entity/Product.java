3b19a558b16a0239ed71a6f282689e6dcecf4330
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-03-26 09:28:56 -0300

    CoCriando Entity Product e Repository ProductRepository

---
+++ product-catalog/src/main/java/one/digitalinnovation/experts/product_catalog/domain/entity/Product.java
@@ -0,0 +1,23 @@
+package one.digitalinnovation.experts.product_catalog.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private Integer amount;
+
+}
